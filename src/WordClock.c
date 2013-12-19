70da430e5af2997224aa741a19ffb06cc1109b58
Travis La Marr <exiva@exiva.net>
2013-12-19 01:40:11 -0500

    Cleaning up

--- src/WordClock.c
+++ src/WordClock.c
@@ -3,6 +3,7 @@
 static Window *window;
 static TextLayer *text_layer;
 
+static const char* _ITIS = "it is";
 //hours
 static const char* _HONE = "one";
 static const char* _HTWO = "two";
@@ -18,17 +19,21 @@ static const char* _HELEVEN = "eleven";
 static const char* _HTWELVE = "twelve";
 
 //minutes
-static char* _MFIVE = "five";
+static const char* _MFIVE = "five";
 static const char* _MTEN = "ten";
 static const char* _MQUARTER = "quarter";
 static const char* _MTWENTY = "twenty";
 static const char* _MTWENTYFIVE = "twenty five";
 static const char* _MHALF = "half";
 
-// //misc
+//misc
 static const char* _PAST = "past";
 static const char* _TO = "to";
 static const char* _OCLOCK = "o'clock";
+
+const char* tminute = NULL;
+const char* thour = NULL;
+const char* tense = NULL;
 
 static void window_load(Window *window) {
 }
@@ -43,90 +48,77 @@ static void deinit(void) {
 
 
 static void test(struct tm* t) {
-  int hour = t->tm_hour;
+  int hour = t->tm_hour % 12;
   int minute = t->tm_min;
 
-  // char* tminute;
-  // char* thour;
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Minute: %i", minute);
-
   if (minute >= 5 && minute <= 9) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", _MFIVE);
+    tminute = _MFIVE;
   } else if (minute >= 10 && minute <= 14) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", _MTEN);
+    tminute = _MTEN;
   } else if (minute >= 15 && minute <= 19) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", _MQUARTER);
+    tminute = _MQUARTER;
   } else if (minute >= 20 && minute <= 24) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", _MTWENTY);
+    tminute = _MTWENTY;
   } else if (minute >= 25 && minute <= 29) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", _MTWENTYFIVE);
+    tminute = _MTWENTYFIVE;
   } else if (minute >= 30 && minute <= 34) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", _MHALF);
+    tminute = _MHALF;
   } else if (minute >= 35 && minute <= 39) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", _MTWENTYFIVE);
+    tminute = _MTWENTYFIVE;
     hour=hour+1;
   } else if (minute >= 40 && minute <= 44) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", _MTWENTY);
+    tminute = _MTWENTY;
     hour=hour+1;
   } else if (minute >= 45 && minute <= 49) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", _MQUARTER);
+    tminute = _MQUARTER;
     hour=hour+1;
   } else if (minute >= 50 && minute <= 54) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", _MTEN);
+    tminute = _MTEN;
     hour=hour+1;
   } else if (minute >= 55) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", _MFIVE);
+    tminute = _MFIVE;
     hour=hour+1;
   }
   
-  if (minute == 0) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",  _OCLOCK);
-  }
-  if (minute >= 1 && minute <= 30) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",  _PAST);
+  // if (minute == 0) {
+  //   APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",  _OCLOCK);
+  // } else {
+
+  // }
+
+  if (minute >= 1 && minute <= 34) {
+    tense = _PAST;
   } else {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",  _TO);
+    tense = _TO;
   }
 
-  switch(hour % 12){
-    case 1:
-     APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", _HONE);
-      break;
-    case 2:
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",  _HTWO);
-      break;
-    case 3:
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",  _HTHREE);
-      break;
-    case 4:
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", _HFOUR);
-      break;
-    case 5:
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", _HFIVE);
-      break;
-    case 6:
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",  _HSIX);
-      break;
-    case 7:
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",  _HSEVEN);
-      break;
-    case 8:
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",  _HEIGHT);
-      break;
-    case 9:
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",  _HNINE);
-      break;
-    case 10:
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",  _HTEN);
-      break;
-    case 11:
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",  _HELEVEN);
-      break;
-    case 12:
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",  _HTWELVE);
-      break;
+  if (hour == 1) {
+    thour = _HONE;
+  } else if (hour == 2) {
+    thour = _HTWO;
+  } else if (hour == 3) {
+    thour = _HTHREE;
+  } else if (hour == 4) {
+    thour = _HFOUR;
+  } else if (hour == 5) {
+    thour = _HFIVE;
+  } else if (hour == 6) {
+    thour = _HSIX;
+  } else if (hour == 7) {
+    thour = _HSEVEN;
+  } else if (hour == 8) {
+    thour = _HEIGHT;
+  } else if (hour == 9) {
+    thour = _HNINE;
+  } else if (hour == 10) {
+    thour = _HTEN;
+  } else if (hour == 11) {
+    thour = _HELEVEN;
+  } else if (hour == 12) {
+    thour = _HTWELVE;
   }
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "%s %s %s %s", _ITIS, tminute, tense, thour );
 
   if (minute % 10 == 0 || minute % 10 == 5) {
     APP_LOG(APP_LOG_LEVEL_DEBUG, "x x x x");
